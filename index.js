a2a6a27dea268491147cc5b806c88b3e232dbdc5
Le Huynh <lhuynh100@gmail.com>
2022-10-03 14:04:16 -0500

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+if (someNumber < 400) {
+return "This one is on me!";
+} 
+else if (someNumber > 400 && someNumber < 2000 ) {
+return "That will be twenty bucks.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (someNumber > 2500) {
+  return "No can do.";
+
+} 
+else if (someNumber > 2000 ) {
+  return "I will gladly take your thirty bucks.";
+
+
 }
 
-function switchOnCharmFromTip(){
+}
+
+function ternaryCheckCity(city){
   // Write your code here!
+if (city === "NYC" ) {
+return "Ok, sounds good.";
+}
+else { 
+  return "No go.";
+}
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+if (tip === "generous") {
+  return "Thank you so much." 
+}  
+else if (tip === "not as generous") {
+  return "Thank you.";
+}
+else {
+  return "Bye.";
+}
 }
